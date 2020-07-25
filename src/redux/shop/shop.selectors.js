9bd7e6e403b62c38cb89ecc05e1acee7def033a3
Patrik Hägg <patrik_hagg@hotmail.com>
2020-07-25 20:21:32 +0200

    Update Data Flow In Our App

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,12 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+export const selectCollectionForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections [key])
+);
+
+
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   collections => Object.keys(collections).map(key => collections[key])
