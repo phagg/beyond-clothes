2dbbf0f2e4b6ab9b298b948dce895b303e0c43cb
Patrik Hägg <patrik_hagg@hotmail.com>
2020-07-30 19:35:34 +0200

    Add Stripe-button, StripeCheckout

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51H8sOuFBrTQ4JsZ7bKCCyjLamH3vySKFnWiw6kVJruPXOupjtSt2PFRbBDauAjxIYS2XvetYYemEKb3YFZTvMHtx00c18HTvjw';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='BEYOND clothes Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
